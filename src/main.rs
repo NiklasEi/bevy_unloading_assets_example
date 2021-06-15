8b713e47826515c3d3357fd6dc7ce5866b27a24b
Niklas Eicker <git@nikl.me>
2021-06-15 18:29:57 +0200

    check the state of all three asset handles

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,48 @@ use bevy::asset::{HandleId, LoadState};
 use bevy::prelude::*;
 use std::time::Duration;
 
+fn check_asset_handles(
+    asset_server: Res<AssetServer>,
+    audio: Res<AudioAssets>,
+    textures: Res<TextureAssets>,
+) {
+    println!(
+        "AssetStates: audio {:?}, player {:?}, tree {:?}",
+        asset_server.get_load_state(audio.background.id),
+        asset_server.get_load_state(textures.player.id),
+        asset_server.get_load_state(textures.tree.id)
+    )
+}
+
+fn spawn(
+    mut commands: Commands,
+    textures: Res<TextureAssets>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(textures.player.clone().into()),
+        ..Default::default()
+    });
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(textures.tree.clone().into()),
+        transform: Transform::from_translation(Vec3::new(100., 10., 0.)),
+        ..Default::default()
+    });
+}
+
+fn play_audio(
+    audio: Res<Audio>,
+    audio_assets: Res<AudioAssets>,
+    mut timer: Local<AudioTimer>,
+    time: Res<Time>,
+) {
+    timer.timer.tick(time.delta());
+    if timer.timer.just_finished() {
+        audio.play(audio_assets.background.clone());
+    }
+}
+
 fn start_loading(mut commands: Commands, asset_server: Res<AssetServer>) {
     let mut handle_ids = vec![];
     handle_ids.push(asset_server.load_untyped("textures/player.png").id);
@@ -29,44 +71,6 @@ fn check_loading(
         });
         commands.remove_resource::<LoadingHandles>();
         state.set(AppState::Game).expect("Failed to set next State");
-    }
-}
-
-fn spawn(
-    mut commands: Commands,
-    textures: Res<TextureAssets>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(SpriteBundle {
-        material: materials.add(textures.player.clone().into()),
-        ..Default::default()
-    });
-    commands.spawn_bundle(SpriteBundle {
-        material: materials.add(textures.tree.clone().into()),
-        transform: Transform::from_translation(Vec3::new(100., 10., 0.)),
-        ..Default::default()
-    });
-}
-
-fn play_audio(
-    audio: Res<Audio>,
-    audio_assets: Res<AudioAssets>,
-    mut timer: Local<AudioTimer>,
-    time: Res<Time>,
-    asset_server: Res<AssetServer>,
-) {
-    timer.timer.tick(time.delta());
-    if asset_server.get_load_state(audio_assets.background.clone()) != LoadState::Loaded {
-        println!(
-            "audio handle not loaded! Has state: {:?}",
-            asset_server.get_load_state(audio_assets.background.clone())
-        );
-    } else {
-        println!("Audio is loaded");
-    }
-    if timer.timer.just_finished() {
-        audio.play(audio_assets.background.clone());
     }
 }
 
@@ -108,6 +112,10 @@ fn main() {
         .add_system_set(SystemSet::on_enter(AppState::Load).with_system(start_loading.system()))
         .add_system_set(SystemSet::on_update(AppState::Load).with_system(check_loading.system()))
         .add_system_set(SystemSet::on_enter(AppState::Game).with_system(spawn.system()))
-        .add_system_set(SystemSet::on_update(AppState::Game).with_system(play_audio.system()))
+        .add_system_set(
+            SystemSet::on_update(AppState::Game)
+                .with_system(play_audio.system())
+                .with_system(check_asset_handles.system()),
+        )
         .run();
 }
