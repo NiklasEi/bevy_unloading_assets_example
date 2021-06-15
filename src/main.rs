89b0096aad70935b452d8d6b6dada7a32b92a9c6
Niklas Eicker <git@nikl.me>
2021-06-15 18:33:48 +0200

    Remove audio (not needed for minimal example)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 use bevy::asset::{HandleId, LoadState};
 use bevy::prelude::*;
-use std::time::Duration;
 
-fn check_asset_handles(
-    asset_server: Res<AssetServer>,
-    audio: Res<AudioAssets>,
-    textures: Res<TextureAssets>,
-) {
+fn check_asset_handles(asset_server: Res<AssetServer>, textures: Res<TextureAssets>) {
     println!(
-        "AssetStates: audio {:?}, player {:?}, tree {:?}",
-        asset_server.get_load_state(audio.background.id),
+        "AssetStates: player {:?}, tree {:?}",
         asset_server.get_load_state(textures.player.id),
         asset_server.get_load_state(textures.tree.id)
     )
@@ -32,23 +26,10 @@ fn spawn(
     });
 }
 
-fn play_audio(
-    audio: Res<Audio>,
-    audio_assets: Res<AudioAssets>,
-    mut timer: Local<AudioTimer>,
-    time: Res<Time>,
-) {
-    timer.timer.tick(time.delta());
-    if timer.timer.just_finished() {
-        audio.play(audio_assets.background.clone());
-    }
-}
-
 fn start_loading(mut commands: Commands, asset_server: Res<AssetServer>) {
     let mut handle_ids = vec![];
     handle_ids.push(asset_server.load_untyped("textures/player.png").id);
     handle_ids.push(asset_server.load_untyped("textures/tree.png").id);
-    handle_ids.push(asset_server.load_untyped("audio/background.ogg").id);
 
     commands.insert_resource(LoadingHandles {
         handles: handle_ids,
@@ -62,9 +43,6 @@ fn check_loading(
     loading_handles: Res<LoadingHandles>,
 ) {
     if asset_server.get_group_load_state(loading_handles.handles.clone()) == LoadState::Loaded {
-        commands.insert_resource(AudioAssets {
-            background: asset_server.get_handle("audio/background.ogg"),
-        });
         commands.insert_resource(TextureAssets {
             player: asset_server.get_handle("textures/player.png"),
             tree: asset_server.get_handle("textures/tree.png"),
@@ -89,22 +67,6 @@ struct TextureAssets {
     player: Handle<Texture>,
 }
 
-struct AudioAssets {
-    background: Handle<AudioSource>,
-}
-
-struct AudioTimer {
-    timer: Timer,
-}
-
-impl Default for AudioTimer {
-    fn default() -> Self {
-        let mut timer = Timer::new(Duration::from_secs(31), true);
-        timer.tick(Duration::from_millis(30999));
-        Self { timer }
-    }
-}
-
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
@@ -113,9 +75,7 @@ fn main() {
         .add_system_set(SystemSet::on_update(AppState::Load).with_system(check_loading.system()))
         .add_system_set(SystemSet::on_enter(AppState::Game).with_system(spawn.system()))
         .add_system_set(
-            SystemSet::on_update(AppState::Game)
-                .with_system(play_audio.system())
-                .with_system(check_asset_handles.system()),
+            SystemSet::on_update(AppState::Game).with_system(check_asset_handles.system()),
         )
         .run();
 }
